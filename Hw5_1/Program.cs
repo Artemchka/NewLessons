3a523797aa365525e447880635a35623b1b05d2c
Artem <shiryaev.artemi@gmail.com>
2023-03-05 21:26:11 +0700

    Добавил решение лекционных задач

--- Hw5_1/Program.cs
+++ Hw5_1/Program.cs
@@ -1,33 +1,35 @@
 ﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-void FillArray(int[] array)
-{
-    for (int i=0;i<array.Length;i++)
-        array[i] = new Random().Next(99,999);
-}
 
-void PrintArray(int[] array)
+
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int count = 0;
+
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+void FillArrayRandomNumbers(int[] numbers)
 {
-    foreach (var el in array)
-        Console.Write($"{el} ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+    }
+    Console.Write("]");
     Console.WriteLine();
 }
-
-
-
-int[] array = new int[12];
-int count =0;
-
-void Chetn(int[] array)
-{
-for (int i=0;i<array.Length; i++)
-if(array[i]%2==0)
- count++;
-    return;
-    Console.WriteLine();
-}
-
-FillArray(array);
-PrintArray(array);
-Chetn(array);
---
+++ sem6_1/Program.cs
@@ -0,0 +1,46 @@
+﻿// Создаем двухмерный массив
+String [,] table = new string[2, 5];  //задаем массив таблицей: первое число количество строк, второе столбцов (индексы начинают меняться с 0)
+/*table [1, 2] = "слово";
+
+for (int rows =0; rows < 2; rows++)
+{
+    for(int colums =0; colums < 5; colums++)
+    {
+        Console.WriteLine($"-{table[rows, colums]}-");
+    }
+}
+*/
+
+
+
+
+void PrintArray(int[,] matrix)
+{
+for (int rows =0; rows < 3; rows++) // for (int rows =0; rows < matrix.GetLength(0); rows++)
+{
+    for(int colums =0; colums < 4; colums++) // for (int rows =0; rows < matrix.GetLength(1); rows++)
+    {
+        Console.Write($"{matrix[rows, colums]} ");
+    }
+Console.WriteLine();
+}
+}
+
+void Filla(int[,] matrix)
+{
+for (int rows =0; rows < 3; rows++) // for (int rows =0; rows < matrix.GetLength(0); rows++)
+ {
+    for(int colums =0; colums < 4; colums++) // for (int rows =0; rows < matrix.GetLength(1); rows++)
+    {
+        matrix[rows, colums] = new Random().Next(1, 10);
+    }
+ }
+}
+
+
+int [,] matrix = new int[3, 4];
+PrintArray(matrix);
+Console.WriteLine();
+
+Filla(matrix);
+PrintArray(matrix);
