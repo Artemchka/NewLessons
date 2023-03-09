0e67030633f65ff1c5b0b7476fffc29aa718f371
Artem <shiryaev.artemi@gmail.com>
2023-03-09 14:50:52 +0700

    Решили задачу 3

---
+++ sem7_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу которая будет преобразовывать десятичное число в двоичное
+
+void Change(int number)
+{
+  string i="";
+
+  while(number > 0)
+  {
+    i = number % 2 + i;
+    number /= 2;
+  }
+Console.WriteLine(i);
+}
+
+Console.WriteLine("Введите десятичное число:\t");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Change(number);
