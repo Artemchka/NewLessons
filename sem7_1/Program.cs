0628188202af97d749844b4aade940cea11b1240
Artem <shiryaev.artemi@gmail.com>
2023-03-09 14:18:30 +0700

    Решил первую задачу семинара

---
+++ sem7_1/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая перевернет одномерный массив 12345 - 54321
+
+void FillArray(int[] array)
+{
+    for (int i=0;i<array.Length;i++)
+        array[i] = new Random().Next(-100,100);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+
+void Reverse(int[] array)
+{
+    int temp;
+    int size = array.Length;
+
+    for (int i=0; i<size/2; i++)
+    {
+        /*temp = array[i];
+        array[i]=array[size-1-i];
+        array[size-1-i]=temp;*/
+        (array[i], array[size-1-i]) = (array[size-1-i], array[i]);
+
+
+
+    }
+}
+int [] array = new int[8];
+FillArray(array);
+PrintArray(array);
+Reverse(array);
+PrintArray(array);
+
