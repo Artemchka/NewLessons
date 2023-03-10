eeb2df5ddd0b9438ff48359d7ca3118fa787368c
Artem <shiryaev.artemi@gmail.com>
2023-03-10 17:57:06 +0700

    Решил вторую задачу семинара

---
+++ Sem7_1_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив размера м на н, каждый элемент в массиве находится по формуле а=м+н Выведите полученный массив на экран
+
+void FillArray(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+
+    for (int j=0; j<array.GetLength(1);j++)
+    array[i,j] = i+j;
+}
+
+void PrintArray(int[,] array)
+{
+     for(int i=0; i < array.GetLength(0); i++)
+     {
+    for ( int j=0; j<array.GetLength(1);j++)
+     Console.Write($"{array[i,j],3} \t");
+    Console.WriteLine();
+     }
+
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
