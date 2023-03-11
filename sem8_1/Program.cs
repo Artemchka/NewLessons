13b633b5587c484658184b75aeb17510639f9e2f
Artem <shiryaev.artemi@gmail.com>
2023-03-11 11:33:09 +0700

    Решил первую задачу 8 семинара

---
+++ sem8_1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку массива (Задайте двумерный массив.  Напишите программу которая заменяет все строки на столбцы)
+
+
+void FillArray(int[,] array)
+{
+    for(int i=0; i<array.GetLength(0); i++)
+    for (int j=0; j<array.GetLength(1);j++)
+    array[i, j] = Convert.ToInt32(new Random().Next(-100, 100));
+}
+
+void PrintArray(int[,] array)
+{
+     for(int i=0; i <array.GetLength(0) ; i++)
+     {
+    for ( int j=0; j<array.GetLength(1);j++)
+
+     Console.Write($"{array[i,j],3} \t");
+    Console.WriteLine();
+     }
+}
+
+
+void ChangeMass(int[,] array)
+{
+    for (int j=0; j<array.GetLength(1);j++)
+    {
+    (array[0, j], array[array.GetLength(0) -1,j]) = (array[array.GetLength(0) -1, j], array[0, j]);
+    } 
+}
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int cols = Convert.ToInt32(Console.ReadLine());
+int [,] array = new int [rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeMass(array);
+PrintArray(array);
